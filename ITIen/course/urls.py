eaea6bd988a0cb3b8e820738adab2f9bf1b7f8c2
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-05-05 00:16:07 +0300

    Basic Style Applied
    - Added style for trainee
    - Added style for course
    - Added Nav bar for basic style

--- ITIen/course/urls.py
+++ ITIen/course/urls.py
@@ -4,7 +4,7 @@ from course.views import *
 urlpatterns = [
     path('', courseList, name="Course_list"),
     path('add/', courseAdd, name="Course_add"),
-    path('update/<int:id>', courseUpdate, name="Course_update"),
-    path('delete/<int:id>', courseDelete, name="Course_delete"),
+    path('update/<int:id>/', courseUpdate, name="Course_update"),
+    path('delete/<int:id>/', courseDelete, name="Course_delete"),
     
 ]
