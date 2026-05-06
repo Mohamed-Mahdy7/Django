3186eb7572d757b3a424dfbf933cbc787b2b8da3
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-05-07 01:20:01 +0300

    implemented Add, Update, Delete logic

--- ITIen/course/views.py
+++ ITIen/course/views.py
@@ -1,6 +1,5 @@
 from multiprocessing import context
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Course
 
 # Create your views here.
@@ -14,10 +13,30 @@ def courseDetails(request, id):
     return render(request, 'course/details.html', context)
 
 def courseAdd(request):
+    if request.method == "POST":
+        Course.objects.create(
+            name = request.POST["name"],
+            code = request.POST["code"],
+            track = request.POST["track"]
+        )
+        return redirect("Course_list")
     return render(request, "course/add.html")
 
 def courseUpdate(request, id):
+    course = Course.objects.get(pk=id)
+    if request.method == "POST":
+        course.name = request.POST.get("name")
+        course.code = request.POST.get("code")
+        course.track = request.POST.get("track")
+        course.save()
+        return redirect("Course_details", id=course.ID)
+        
     return render(request, 'course/update.html', {"id": id})
 
 def courseDelete(request, id):
+    course = Course.objects.get(pk=id)
+    if request.method == "POST":
+        course.delete()
+        return redirect("Course_list")
+    
     return render(request, 'course/delete.html', {"id": id})
--- ITIen/trainee/views.py
+++ ITIen/trainee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Trainee
 
 # Create your views here.
@@ -15,10 +15,30 @@ def traineeDetails(request, id):
     return render(request, "trainee/details.html", context)
 
 def traineeAdd(request):
+    if request.method == "POST":
+        Trainee.objects.create(
+            name = request.POST["name"],
+            age = request.POST["age"],
+            degree = request.POST["degree"]
+        )
+        return redirect('Trinee_list')
     return render(request, "trainee/add.html")
 
 def traineeUpdate(request, id):
+    trainee = Trainee.objects.get(pk=id)
+    if request.method == "POST":
+        trainee.name = request.POST.get("name")
+        trainee.age = request.POST.get("age")
+        trainee.degree = request.POST.get("degree")
+        trainee.save()
+        return redirect("Trainee_Details", id=trainee.ID)
+    
     return render(request, "trainee/update.html", {"id": id})
 
 def traineeDelete(request, id):
-    return render(request, "trainee/delete.html", {"id": id})
+    trainee = Trainee.objects.get(pk=id)
+    if request.method == "POST":
+        trainee.delete()
+        return redirect('Trinee_list')
+    
+    return render(request, 'trainee/delete.html')
