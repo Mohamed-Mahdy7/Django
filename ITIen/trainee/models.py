d2abed7d7a441b1a2f7628de2533fe262128c48c
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-05-10 15:55:52 +0300

    Created Form & FormModle Views
    -implemented imageField, Course FK for Trainee
    -made Add Function with Form and ModelForm

--- ITIen/trainee/models.py
+++ ITIen/trainee/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+from tkinter import CASCADE
 
+from django.db import models
+from course.models import Course
 # Create your models here.
 
 class Trainee(models.Model):
@@ -7,6 +9,8 @@ class Trainee(models.Model):
     name=models.CharField(max_length=100, null=False)
     age=models.IntegerField(null=False)
     degree=models.DecimalField(decimal_places=2, max_digits=4 ,null=False)
+    image=models.ImageField(upload_to='trainee', blank=True, null=True)
+    course=models.ForeignKey(Course, on_delete=models.PROTECT, default=2)
     
     def __str__(self):
         return self.name
--- ITIen/trainee/views.py
+++ ITIen/trainee/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Trainee
+from course.models import Course
+from .forms import TraineeForm, TraineeFormModel
 
 # Create your views here.
 
@@ -7,22 +9,40 @@ def home(request):
     return render(request, "base.html")
 
 def traineeList(request):
-    context = {"students": Trainee.objects.all()}
+    context = {"trainees": Trainee.objects.all()}
     return render(request, "trainee/trainee.html", context)
 
 def traineeDetails(request, id):
-    context = {"student": Trainee.objects.get(pk=id)}
+    context = {"trainee": Trainee.objects.get(pk=id)}
     return render(request, "trainee/details.html", context)
 
 def traineeAdd(request):
+    context = {"trainees": Trainee.objects.all(), 'form':TraineeFormModel()}
     if request.method == "POST":
-        Trainee.objects.create(
+        form = TraineeFormModel(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('Trinee_list')
+    return render(request, "trainee/add.html", context=context)
+
+def traineeAddForm(request):
+    context = {"trainees": Trainee.objects.all(), 'form':TraineeForm()}
+    if request.method == "POST":
+        form = TraineeForm(data=request.POST, files=request.FILES)        
+        if form.is_valid():
+            print("\nTHIS IS VALID\n")
+            Trainee.objects.create(
             name = request.POST["name"],
             age = request.POST["age"],
-            degree = request.POST["degree"]
-        )
-        return redirect('Trinee_list')
-    return render(request, "trainee/add.html")
+            degree = request.POST["degree"],
+            image = request.FILES["image"],
+            course = Course.objects.get(
+                pk=request.POST["course"]
+            ))
+            return redirect('Trinee_list')
+        else:
+            print(form.errors)
+    return render(request, "trainee/add.html", context=context)
 
 def traineeUpdate(request, id):
     trainee = Trainee.objects.get(pk=id)
